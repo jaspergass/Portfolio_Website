9e67b8157c5ac952b22041765858ccf01ad563e9
jasper <ejg11@sfu.ca>
2023-01-04 17:10:08 -0800

    Working Back to Top Btn

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+//back to top button, from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // from https://codepen.io/koko-hranghlu/pen/vYRzvRo
 
 const gridImages = document.querySelectorAll(".grid > img");
@@ -42,24 +65,3 @@ function showHide(ele) {
   }
 }
 
-//back to top button, from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
